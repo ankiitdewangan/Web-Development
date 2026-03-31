3cf58a7c6f6e7835c5a9df3d2fd26509c39cb28e
ankiiitttt <ankitdewangan225@gmail.com>
2026-03-31 11:31:20 +0530

    JS variables and objects

--- 55 - JS Data types, variables and objects/script.js
+++ 55 - JS Data types, variables and objects/script.js
@@ -1,1 +1,35 @@
 console.log("This is JavaScript");
+
+let a = 5;
+let b = 7;
+console.log(a+b);
+let c = "Ankit"
+console.log(typeof a, typeof b, typeof c);
+
+// console.log(a);
+
+// {
+//     let a = 77;
+//     console.log(a);
+    
+// }
+// console.log(a);
+
+x= "ankit";
+y= 28;
+z= 78.05;
+p= null;
+q= undefined;
+const r = true;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    "name" : "Ankit",
+    "job code" : 1100,
+}
+o.salary = "500crores"
+
+o.is_handsome = true;
+console.log(o);
