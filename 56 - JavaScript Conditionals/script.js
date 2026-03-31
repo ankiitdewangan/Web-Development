1ce13440549975b9b93f1a50dbd1f41a0c77af7c
ankiiitttt <ankitdewangan225@gmail.com>
2026-03-31 15:10:39 +0530

    JavaScript Conditionals

---
+++ 56 - JavaScript Conditionals/script.js
@@ -0,0 +1,35 @@
+console.log("My name is Ankit Dewangan");
+
+let age = 0;
+let grace = 2;
+
+// if (age + grace>= 18) {
+//     console.log("You can drive");
+// }
+// else {
+//     console.log("You cannot drive");
+// }
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace); //exponentiation
+// console.log(age % grace);
+
+// age += grace //increment operator
+// console.log(age);
+
+// if(age==18){
+//     console.log("you can drive");
+// }
+
+if(age==18){
+    console.log("you can drive");
+}
+else if(age == 0){
+    console.log("are you kidding");
+}
+else{
+    console.log("you cannot drive");
+    
+}
