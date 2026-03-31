53dea93709545b65afbfbdd73107bde0b48201ce
ankiiitttt <ankitdewangan225@gmail.com>
2026-03-31 18:12:17 +0530

    JavaScript Loops

---
+++ 57 - JavaScript Loops/index.js
@@ -0,0 +1,35 @@
+console.log("I am a tutorial on Loops");
+
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name : "Ankit",
+//     Role : "Director",
+//     Company : "Ankit Enterprises"
+// }
+
+// for (const key in obj) {
+//     {
+//         const element = obj[key];
+//         console.log(key, element);
+//     }
+// }
+
+// for(const c of "ANKIT"){
+//     console.log(c);
+// }
+
+// let i = 0
+// while (i<10) {
+//     i++;
+//     console.log(i);
+// }
+
+let i = 11;
+do {
+    console.log(i);
+    i++;
+} while (i<10);
