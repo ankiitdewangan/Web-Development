5be16121311d63097b37b4934119aea5ae93929f
ankiiitttt <ankitdewangan225@gmail.com>
2026-04-01 23:37:38 +0530

    JavaScript Functions

---
+++ 58 - JavaScript Functions/index.js
@@ -0,0 +1,20 @@
+// function nice(name) {
+//     console.log("Hey " + name + " you are good");
+//     console.log("Hey " + name + " you are good");
+//     console.log("Hey " + name + " you are nice");
+//     console.log("Hey " + name + " your outfit is nice");
+// }
+// nice("Ankit Dewangan")
+
+// function sum(a, b) {
+//     // console.log(a+b);
+//     return a+b
+// }
+// result = sum(9,6)
+
+// console.log("The sum of these numbers is: ", result)
+
+const func1 = (x)=> {
+    console.log("I am an Arrow function" , x) 
+}
+func1(67)
