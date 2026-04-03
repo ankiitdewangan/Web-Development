222b18a722480a1c322966d6c36d340ca42d168a
ankiiitttt <ankitdewangan225@gmail.com>
2026-04-03 23:37:24 +0530

    JavaScript Strings

---
+++ 60 - JavaScript Strings/index.js
@@ -0,0 +1,26 @@
+console.log("my name is Ankit dewangan");
+let a = "ANKIT"
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log("length of a = ", a.length);
+
+let real_name = "Ankit"
+let hobby = "Travelling"
+console.log("His name is " + real_name + " and his hobby is " + hobby);
+
+console.log(`His name is ${real_name} and his hobby is ${hobby}`);
+let b = "AnkitDewangan"
+
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+
+// console.log(b.slice(1,7));
+console.log(b.replace("Dewangan", "Bhai"));
+
+console.log(b.concat(a, "DEWANGAN"));
