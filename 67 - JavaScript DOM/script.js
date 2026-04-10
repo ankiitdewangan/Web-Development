a181b17fb22767211604261ca643d0a2e870864d
ankiiitttt <ankitdewangan225@gmail.com>
2026-04-10 23:37:19 +0530

    JavaScript DOM

---
+++ 67 - JavaScript DOM/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+console.log(document.body);
+
+console.log(document.body.childNodes);
+
+console.log(document.body.childNodes[0]);
+console.log(document.body.childNodes[1].childNodes);
+console.log(document.body.childNodes[2]);
+console.log(document.body.childNodes[3]);
+console.log(document.body.childNodes[4]);
+
+console.log(document.body.firstElementChild);
+
+console.log(document.body.firstElementChild.childNodes)
