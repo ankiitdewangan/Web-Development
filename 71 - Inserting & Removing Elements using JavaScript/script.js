4c460be0fc42da128797d1a3515919162e1316f8
ankiiitttt <ankitdewangan225@gmail.com>
2026-04-16 23:32:59 +0530

    Inserting Element using JavaScript

---
+++ 71 - Inserting & Removing Elements using JavaScript/script.js
@@ -0,0 +1,7 @@
+// let div = document.createElement("div")
+// div.innerHTML = "I've been inserted <b> by Ankit </b> "
+// div.setAttribute("class" , "created")
+// document.querySelector(".container").append(div)
+
+let cont =  document.querySelector(".container")
+cont.insertAdjacentHTML("beforeend" , "<b> I am Ankit Dewangan</b>")
