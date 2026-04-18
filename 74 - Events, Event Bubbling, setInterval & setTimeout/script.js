059eafea65f6ac08406ee5a41e8536d733468c3c
ankiiitttt <ankitdewangan225@gmail.com>
2026-04-18 23:56:37 +0530

    Events, Event Bubbling, setInterval & setTimeout

--- 74 - Events, Event Bubbling, setInterval & setTimeout/script.js
+++ 74 - Events, Event Bubbling, setInterval & setTimeout/script.js
@@ -1,4 +1,14 @@
 let button = document.getElementById("btn")
 button.addEventListener("click", ()=>{
-    
+    document.querySelector(".box").innerHTML = 
+    "<b> Yayy you were clicked. </b> Enjoy your Click"
 })
+
+button.addEventListener("contextmenu", ()=>{
+   alert("Dont Hack us by Right Clicking")
+})
+
+document.addEventListener("keydown", (e)=>{
+   console.log(e);
+   
+ })
